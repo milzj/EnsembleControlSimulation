53ea229c6b39c62b727191bcb88e9af269954578
Johannes Milz <johannes.milz@isye.gatech.edu>
2024-06-21 11:42:54 -0400

    Added simulation script.

---
+++ code/simulate_problems.sh
@@ -0,0 +1,21 @@
+date=$(date '+%d-%b-%Y-%H-%M-%S')
+
+declare -a Problems=("harmonic_oscillator" "cubic_oscillator")
+declare -a Problems=("cubic_oscillator")
+
+nrefsamples=$[2**12]
+nreplications="20"
+lbrange="2"
+ubrange="11"
+
+
+for problem in "${Problems[@]}"
+do
+    mkdir -p output/$date/$problem
+
+    python simulate_nominal_problem.py $date $problem > output/$date/$problem/simulation_terminal_output.txt
+    python simulate_reference_problem.py $date $problem $nrefsamples >> output/$date/$problem/simulation_terminal_output.txt
+    python simulate_saa_problem.py $date $problem $nreplications $lbrange $ubrange >> output/$date/$problem/simulation_terminal_output.txt
+    python simulate_criticality_measures.py $date $problem $nrefsamples
+    python plot_rates.py $date $date $problem
+done
